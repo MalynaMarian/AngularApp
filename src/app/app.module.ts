be8a875a20fa3930ad631aec6b9927736eca85fb
Malyna Marian <malina.marian@gmail.com>
2018-10-27 21:19:24 +0300

    add new components and templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyComponent } from './my/my.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyComponent
+    MyComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
